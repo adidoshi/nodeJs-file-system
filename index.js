ca840d2a3f396c6fb9186cd74896ce7889980711
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-11-03 17:59:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  let date = Date().toString();
+  let currentTimestamp = new Date().getTime().toString();
+
+  fs.writeFileSync(
+    "currentDate-time.txt",
+    `${date}, Current Timestamp: ${currentTimestamp}`
+  );
+
+  fs.readFile("currentDate-time.txt", "utf-8", (err, data) => {
+    if (err) throw err;
+    else res.send(data);
+  });
+  // res.send(`${date}, Current Timestamp: ${currentTimestamp}`);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening to port ${port}`);
+});
